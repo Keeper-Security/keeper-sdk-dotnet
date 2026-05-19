553faa737cc504dfa8810a807c2232546f70af50
amandli-ks <amandli@keepersecurity.com>
2026-05-19 11:08:34 +0530

    Added Support for Keeper drive sync down

--- KeeperSdk/vault/KeeperDriveSync.cs
+++ KeeperSdk/vault/KeeperDriveSync.cs
@@ -33,10 +33,10 @@ namespace KeeperSecurity.Vault
             StoreRecordData(kdData, storage);
             StoreFolderRecords(kdData, storage);
             StoreRecords(kdData, storage);
-            ProcessRevokedFolderAccesses(kdData, storage);
             StoreFolderAccesses(kdData, storage);
-            ProcessRevokedRecordAccesses(kdData, storage);
+            ProcessRevokedFolderAccesses(kdData, storage);
             StoreRecordAccesses(kdData, storage);
+            ProcessRevokedRecordAccesses(kdData, storage);
             StoreRecordLinks(kdData, storage);
             StoreFolderSharingStates(kdData, storage);
             StoreRecordSharingStates(kdData, storage);
